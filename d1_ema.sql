bd1f5798c896d4cea9b0df5b3f436d1baef022cb
E Ma <e@Es-iMac.attlocal.net>
2022-07-27 20:14:08 -0400

    7-12

--- d1_ema.sql
+++ d1_ema.sql
@@ -46,44 +46,47 @@ FROM Production.Product
 WHERE Color IS NOT NULL
 
 -- 7. Write a query to retrieve the to the columns ProductID and Name from the Production.Product table filtered by ProductID from 400 to 500 using between
+SELECT ProductID, Name
+FROM Production.Product
+WHERE ProductID BETWEEN 400 AND 500
 
 -- 8. Write a query to retrieve the to the columns  ProductID,
 -- Name and color from the Production.Product table restricted to the colors black and blue
+SELECT ProductID, Name, Color
+FROM Production.Product
+WHERE Color = 'black' OR Color = 'blue'
 
 
 -- 9. Write a query to get a result set on products that begins
 -- with the letter S. 
-
+SELECT *
+FROM Production.Product
+WHERE Name LIKE 'S%'
 
 -- 10. Write a query that retrieves the columns Name and ListPrice
 -- from the Production.Product table. Your result set should look something like the following. Order the result set
-
-
 -- by the Name column. The products name should start with either 'A' or 'S'
-
+SELECT '-- ' + Name, ListPrice
+FROM Production.Product
+WHERE Name LIKE 'A%' OR Name LIKE 'S%'
+ORDER BY Name
 
 -- Name                                            ListPrice
-
-
 -- Adjustable Race                                   0,00
-
-
 -- All-Purpose Bike Stand                       159,00
-
-
 -- AWC Logo Cap                                      8,99
-
-
 -- Seat Lug                                          0,00
-
-
 -- Seat Post                                         0,00
-
-
 
 -- 11. Write a query so you retrieve rows
 -- that have a Name that begins with the letters SPO, but is then not followed by the letter K. After this zero or more letters can exists. Order the result set by the Name column.
-
+SELECT *
+FROM Production.Product
+WHERE Name LIKE 'SPO[^K]%'
+ORDER BY Name
 
 -- 12. Write a query that retrieves the unique combination of
 -- columns ProductSubcategoryID and Color from the Production.Product table. We do not want any rows that are NULL.in any of the two columns in the result. (Hint: Use IsNull)
+SELECT DISTINCT ProductSubcategoryID, Color
+From Production.Product
+Where ProductSubcategoryID IS NOT NULL And Color IS NOT NULL
