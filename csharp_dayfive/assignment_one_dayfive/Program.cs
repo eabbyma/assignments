9b6872325361370326120397b1687f95d768b6b2
E Ma <e@Es-iMac.attlocal.net>
2022-08-03 19:55:12 -0400

    assignment 1

--- csharp_dayfive/assignment_one_dayfive/Program.cs
+++ csharp_dayfive/assignment_one_dayfive/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// them their favorite color, their astrology sign, and their street address number. The result
+// might be something like "Your hacker name is RedGemini480."
+
+
+Console.WriteLine("Enter your favorite color:");
+string color = Console.ReadLine();
+
+Console.WriteLine("Enter your astrology sign:");
+String sign = Console.ReadLine();
+
+Console.WriteLine("Enter your street address number:");
+String num = Console.ReadLine();
+
+Console.WriteLine("Your hacker name is" + color+sign+num);
---
+++ csharp_dayfive/hackerNameGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color:");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("Enter your astrology sign:");
+            String sign = Console.ReadLine();
+
+            Console.WriteLine("Enter your street address number:");
+            String num = Console.ReadLine();
+
+            Console.WriteLine("Your hacker name is" + color + sign + num);
+        }
+    }
+}
